8e5cb907c2faa7a747f34a71405d6a787c71383a
Jim Tatarakis <jimtatarakis@gmail.com>
2019-04-07 06:16:41 -0700

    added some psuedocode and the trivia questions array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,49 @@
+// Trivia Questions Array
+let triviaArr = [
+    {
+        question: 'Riddle me this. Who is the first artist to draw Batman?',
+        answers: ['Rob Shnieder','Bob Kane','George Lucas'],
+        correct: 'Bob Kane'  //might need to change
+    },
+    {
+        question: 'Riddle me this. Who is most known as the voice of Batman?',
+        answers: ['Adam West','Val Kilmer','Kevin Conroy'],
+        correct: 'Kevin Conroy'  //might need to change
+    },
+    {
+        question: 'Riddle me this. Name of the first Robin.',
+        answers: ['Richard Hammon','Dick Grayson','Damian Wayne'],
+        correct: 'Dick Grayson'  //might need to change
+    },
+    {
+        question: 'Riddle me this. Who is the Son of Batman?',
+        answers: ['Richard Grayson','Damian Wayne','Tim Drake'],
+        correct: 'Damian Wayne'  //might need to change
+    },
+    {
+        question: 'Riddle me this. Which Super Hero patrols BloodhavenNightwing',
+        answers: ['Red Robin','Bat Girl','Nightwing'],
+        correct: 'Nightwing'  //might need to change
+    },
+    {
+        question: 'Riddle me this. Which actor is known as the worst Batman ever?',
+        answers: ['George Cloony','Val Kilmer','Christian Bale'],
+        correct: 'George Cloony'  //might need to change
+    },
+    {
+        question: 'Riddle me this. Which is NOT a nickname of Batman?',
+        answers: ['Batsy-Poo','Bats','Caped Detective'],
+        correct: 'Caped Detective'  //might need to change
+    }
+]
+
+
+
 // TODO
 // Make Questions and answers ARRAY, 
-// Randomly select questions
 // display questions
+// Do something when they click an answer.
+// if they get all answers right give alert and clear interval
 
 // TODO
 // Make SCORE counter (resets at loss)
