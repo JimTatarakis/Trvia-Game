08b8642c302d69fb84fc4567b22e239f5408a2ee
Jim Tatarakis <jimtatarakis@gmail.com>
2019-04-12 16:44:35 -0700

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,74 +2,137 @@
 let triviaArr = [
     {
         question: 'Riddle me this. Who is the first artist to draw Batman?',
-        answers: ['Rob Shnieder','Bob Kane','George Lucas'],
+        answers: ['Rob Shnieder', 'Bob Kane', 'George Lucas'],
         correct: 'Bob Kane'  //might need to change
     },
     {
         question: 'Riddle me this. Who is most known as the voice of Batman?',
-        answers: ['Adam West','Val Kilmer','Kevin Conroy'],
+        answers: ['Adam West', 'Val Kilmer', 'Kevin Conroy'],
         correct: 'Kevin Conroy'  //might need to change
     },
     {
         question: 'Riddle me this. Name of the first Robin.',
-        answers: ['Richard Hammon','Dick Grayson','Damian Wayne'],
+        answers: ['Richard Hammon', 'Dick Grayson', 'Damian Wayne'],
         correct: 'Dick Grayson'  //might need to change
     },
     {
         question: 'Riddle me this. Who is the Son of Batman?',
-        answers: ['Richard Grayson','Damian Wayne','Tim Drake'],
+        answers: ['Richard Grayson', 'Damian Wayne', 'Tim Drake'],
         correct: 'Damian Wayne'  //might need to change
     },
     {
         question: 'Riddle me this. Which Super Hero patrols BloodhavenNightwing',
-        answers: ['Red Robin','Bat Girl','Nightwing'],
+        answers: ['Red Robin', 'Bat Girl', 'Nightwing'],
         correct: 'Nightwing'  //might need to change
     },
     {
         question: 'Riddle me this. Which actor is known as the worst Batman ever?',
-        answers: ['George Cloony','Val Kilmer','Christian Bale'],
-        correct: 'George Cloony'  //might need to change
+        answers: ['George Clooney', 'Val Kilmer', 'Christian Bale'],
+        correct: 'George Clooney'  //might need to change
     },
     {
         question: 'Riddle me this. Which is NOT a nickname of Batman?',
-        answers: ['Batsy-Poo','Bats','Caped Detective'],
+        answers: ['Batsy-Poo', 'Bats', 'Caped Detective'],
         correct: 'Caped Detective'  //might need to change
     }
 ]
-
-
-
-// TODO
-// Make Questions and answers ARRAY, 
-// display questions
-// Do something when they click an answer.
-// if they get all answers right give alert and clear interval
-
-// TODO
-// Make SCORE counter (resets at loss)
-// DISPLAY score counter
-
-
-
+let questNum = 0;
+let score = 0;
+// Displays first question to User.
+$(document).ready(function () {
+    $('#question').append('<h2>' + triviaArr[questNum].question + '</h2>');
+    for (let i = 0; i < triviaArr[questNum].answers.length; i++) {
+        var logo = $('<div class = "logo" id=' + i + '></div>')
+        var h3 = $('<h3 class = "answer">' + triviaArr[questNum].answers[i] + '</h3>');
+        $('#answers').append(logo);
+        $('#' + i).append(h3);
+    }
+});
 
 // Timer
 let counter = 30;
 
-timeLeft = () =>{
+timeLeft = () => {
     counter--;
 
     $('#timer').html('Time Remaining: ' + counter + ' seconds');
 
-    if(counter === 0){
+    if (counter === 0) {
         alert('You lose!')
         counter = 30;
         $('#timer').html('Time Remaining: ' + counter + ' seconds')
-        // initialize reset game
+
+        alert('You lose, maybe next time detective!')
+        questNum = 0;
+        score = 0;
+        $('#question').empty();
+        $('#answers').empty();
+        $('#question').append('<h2>' + triviaArr[questNum].question + '</h2>');
+        for (let i = 0; i < triviaArr[questNum].answers.length; i++) {
+            var logo = $('<div class = "logo" id=' + i + '></div>')
+            var h3 = $('<h3 class = "answer">' + triviaArr[questNum].answers[i] + '</h3>');
+            $('#answers').append(logo);
+            $('#' + i).append(h3);
+        }
+        counter = 30;
     }
 }
+let intervalID = 0;
+timeCounter = () => {
 
-timeCounter = () =>{
-setInterval(timeLeft,1000);
+    intervalID = setInterval(timeLeft, 1000);
 }
 // Timer End
+timeCounter();
 
+$('#answers').on('click', 'h3.answer', function () {
+    // If Correct....
+    // test
+    console.log($(this).attr('data-answer'));
+    console.log($(this).text());
+    console.log(triviaArr[questNum].correct);
+    // test end
+    if ($(this).text() === triviaArr[questNum].correct) {
+        // Increase Question var, clear out old quest and ans populate the next one. reset timer.
+        questNum++;
+        score++;
+        // Has the last question been answered?
+        // Yes it has.
+        if (score === 7) {
+            clearInterval(intervalID);
+            $('#question').empty();
+            $('#answers').empty();
+            alert('You win! A great Detective you are!')
+
+        }
+        // No it hasn't.
+        else {
+            $('#question').empty();
+            $('#answers').empty();
+            $('#question').append('<h2>' + triviaArr[questNum].question + '</h2>');
+            for (let i = 0; i < triviaArr[questNum].answers.length; i++) {
+                var logo = $('<div class = "logo" id=' + i + '></div>')
+                var h3 = $('<h3 class = "answer">' + triviaArr[questNum].answers[i] + '</h3>');
+                $('#answers').append(logo);
+                $('#' + i).append(h3);
+            }
+            counter = 30;
+        }
+    }
+    // If Wrong...
+    else {
+        alert('You lose, maybe next time detective!')
+        questNum = 0;
+        score = 0;
+        $('#question').empty();
+        $('#answers').empty();
+        $('#question').append('<h2>' + triviaArr[questNum].question + '</h2>');
+        for (let i = 0; i < triviaArr[questNum].answers.length; i++) {
+            var logo = $('<div class = "logo" id=' + i + '></div>')
+            var h3 = $('<h3 class = "answer">' + triviaArr[questNum].answers[i] + '</h3>');
+            $('#answers').append(logo);
+            $('#' + i).append(h3);
+        }
+        counter = 30;
+    }
+});
